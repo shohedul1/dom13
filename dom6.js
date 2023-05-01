eae67cc9681de9d7a9f5f6753d51f9e2e508f0be
shohedul-islam <shohedulislam564@gmail.com>
2023-05-01 17:39:25 +0600

    first commit

---
+++ dom6.js
@@ -0,0 +1,32 @@
+let form = document.getElementById('form')
+
+form.addEventListener('submit',function(e){
+    e.preventDefault()
+    //console.log(this.elements[0])
+    //console.log(this['email'])
+    let isValid = false
+    let formData = {};
+    [...this.elements].forEach(el =>{
+        if(el.type !== 'submit'){
+           isValid = validator(el)
+           if(isValid){
+            formData[el.name] = el.value
+           }else{
+            el.classList.add('is-invalid')
+            alert(`${el.name} is Required`)
+           }
+        }
+     })
+     if(isValid){
+        console.log(formData)
+        this.reset ()
+     }
+     
+})
+
+function validator(el){
+    if(!el.value){
+       return false
+    }
+    return true
+}
